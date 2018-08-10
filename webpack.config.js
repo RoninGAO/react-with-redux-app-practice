2ac095f5c8853cc868b0fce4f7ee2a4c625ef8cc
RoninGAO <minkou.lee@gmail.com>
2018-08-10 17:25:56 +0800

    webpack include

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,11 @@
+/*
 var webpack = require('webpack');
 var path = require('path');
 
 var BUILD_DIR = path.resolve(__dirname, 'src/client/public');
 var APP_DIR = path.resolve(__dirname, 'src/client/app');
 
-var config = {
+let config = {
   entry: APP_DIR + '/index.jsx',
   output: {
     path: BUILD_DIR,
@@ -13,3 +14,27 @@ var config = {
 };
 
 module.exports = config;
+*/
+// var webpack = require("webpack")
+var path = require("path")
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.join(__dirname, 'dist', 'assets'),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        loader: "babel-loader",
+        query: {
+          presets: ['env', 'stage-0', 'react']
+        }
+
+      }
+    ]
+  }
+}
